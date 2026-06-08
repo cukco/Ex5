0a5c5f1f649b681f50e48e09363c0674f0aa7818
Hưng Đinh <hung.dq2416937@sis.hust.edu.vn>
2026-06-08 10:26:14 +0700

    Ex5 SS8

--- Ex5.sql
+++ Ex5.sql
@@ -1,50 +1,30 @@
---Part2--
-create index idx_email on customers(email);
+create or replace procedure update_employee_status(
+    p_emp_id INT,
+    out p_status TEXT
+) language plpgsql
+as $$
+    declare
+        v_salary numeric(10,2);
+    begin
+        select salary into v_salary from employees
+        where id=p_emp_id;
 
-create index idx_city on customers using hash(city);
+        if v_salary is null then
+            raise exception 'Employee not found';
+        end if;
 
-create index idx_category on products using gin(category);
+        if v_salary < 5000 then
+            p_status:='Junior';
+        elsif v_salary between 5000 and 10000 then
+            p_status:='Mid-level';
+        else
+            p_status:='Senior';
+        end if;
+    exception
+        when others then
+            raise notice 'Xảy ra lỗi: %',SQLERRM;
+            rollback;
+    end;
+$$;
 
-create index idx_price on products using gist(price);
-
---Part3--
-explain analyze select *from customers
-                where email is not null;
-
-explain analyse select *from products
-                where 'Electronics'=any(category);
-
-explain analyze select *from products
-                where price between 500 and 1000;
-
---Part4--
-create index idx_orderdate on orders(order_date);
-
-cluster orders using idx_orderdate;
-
---Part5--
-create view v1 as
-select full_name from customers
-inner join orders on customers.customer_id = orders.customer_id
-inner join products on orders.product_id = products.product_id
-group by orders.customer_id, full_name
-order by sum(quantity*price) desc limit 3;
-
-select * from v1;
-
-create view v2 as
-select product_name, sum(quantity*price) as total_revenue from products
-inner join orders on products.product_id = orders.product_id
-group by product_name;
-
-select * from v2;
-
---Part6--
-CREATE VIEW v_customer_city AS
-SELECT customer_id, full_name, city FROM customers
-where city='Hà Nội' WITH CHECK OPTION;
-
-update v_customer_city
-set city='Beijing' where customer_id=1;
-
-
+call update_employee_status(9,null);
